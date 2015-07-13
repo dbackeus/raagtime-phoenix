4305a9e36babddaa44cfbb38aec6fcf061401c6f
David Backeus <duztdruid@gmail.com>
2015-07-13 15:43:34 +0200

    Improve show view

--- web/views/raga_view.ex
+++ web/views/raga_view.ex
@@ -5,7 +5,6 @@ defmodule Raagtime.RagaView do
     raw "<img class='sort-arrow' src='/images/sort_arrow.gif'></img>"
   end
 
-
   @time_map %{
     "1" => "06-09",
     "2" => "09-12",
@@ -19,7 +18,6 @@ defmodule Raagtime.RagaView do
   }
 
   def pretty_time(nil), do: ""
-
   def pretty_time(time) do
     Map.get(@time_map, time)
   end
@@ -37,16 +35,54 @@ defmodule Raagtime.RagaView do
 
   def chakra_sort_value(nil), do: "zzz"
   def chakra_sort_value(""), do: "zzz"
-
   def chakra_sort_value(chakra) do
-    actual = chakra |> String.split("_") |> List.last
-    Map.get @chakra_positions, actual
+    Map.get @chakra_positions, actual_chakra(chakra)
   end
 
   def chakra_icon(nil), do: ""
-
   def chakra_icon(chakra) do
-    chakra = chakra |> String.split("_") |> List.last
-    raw "<img class='chakra-icon' src='/images/chakra-icon-#{chakra}.png'></img>"
+    raw "<img class='chakra-icon' src='/images/chakra-icon-#{actual_chakra chakra}.png'></img>"
+  end
+
+  def chakra_image(chakra) do
+    raw "<img class='chakra-image' src='/images/chakra-#{actual_chakra chakra}.jpg'></img>"
+  end
+
+  @chakra_names %{
+    "left_mooladhara" => "Left Mooladhara",
+    "mooladhara" => "Mooladhara",
+    "right_mooladhara" => "Right Mooladhara",
+    "left_swadisthan" => "Left Swadisthan",
+    "swadisthan" => "Swadisthan",
+    "right_swadisthan" => "Right Swadisthan",
+    "left_nabhi" => "Left Nabhi",
+    "nabhi" => "Nabhi",
+    "right_nabhi" => "Right Nabhi",
+    "void" => "Void",
+    "left_heart" => "Left Heart",
+    "heart" => "Heart",
+    "right_heart" => "Right Heart",
+    "left_vishuddhi" => "Left Vishuddhi",
+    "vishuddhi" => "Vishuddhi",
+    "right_vishuddhi" => "Right Vishuddhi",
+    "left_agnya" => "Left Agnya",
+    "agnya" => "Agnya",
+    "right_agnya" => "Right Agnya",
+    "left_sahasrara" => "Left Sahastrara",
+    "sahasrara" => "Sahastrara",
+    "right_sahasrara" => "Right Sahastrara"
+  }
+
+  def pretty_chakra(chakra) do
+    Map.get @chakra_names, chakra
+  end
+
+  def present?(nil), do: false
+  def present?(""), do: false
+  def present?(_), do: true
+
+  # "left_nabhi" |> "nabhi"
+  defp actual_chakra(chakra) do
+    chakra |> String.split("_") |> List.last
   end
 end
