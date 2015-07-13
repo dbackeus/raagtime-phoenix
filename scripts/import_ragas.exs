a60f6344412f2f7adbf3bf96e6ded70d27f171b2
David Backeus <duztdruid@gmail.com>
2015-07-13 13:34:56 +0200

    Cleaner raga import

--- scripts/import_ragas.exs
+++ scripts/import_ragas.exs
@@ -1,18 +1,21 @@
 alias Raagtime.Raga
 alias Raagtime.Repo
+import IO
 
 {:ok, file} = File.read("db/ragas.json")
 {:ok, ragas} = Poison.Parser.parse(file, keys: :atoms)
 
 importer = fn (raga) ->
-  IO.puts "Importing #{raga.title}"
+  puts "Importing #{raga.title}"
+
   raga = Map.put(raga, :slug, Map.get(raga, :url))
   changeset = Raga.changeset(%Raga{}, raga)
+
   if changeset.valid? do
     Repo.insert!(changeset)
   else
-    IO.puts "Failed to insert raga #{raga.title}"
-    IO.puts changeset.errors
+    puts "Failed to insert raga #{raga.title}"
+    puts changeset.errors
   end
 end
 
