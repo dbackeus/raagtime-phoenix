32bb344b149513732d6b5352455217aae1b8f3ae
David Backeus <duztdruid@gmail.com>
2015-07-14 10:54:35 +0200

    Not using the page controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule Raagtime.PageController do
   use Raagtime.Web, :controller
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
+  # def index(conn, _params) do
+  #   render conn, "index.html"
+  # end
 end
